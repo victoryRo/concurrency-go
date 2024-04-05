75f61c767e884425cb8d4ee2f84db67b06168b7b
Victor <victoriess77@gmail.com>
2024-04-05 10:07:57 -0500

    Removed duplicate code

--- main.go
+++ main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	wptwo "github.com/victoryRo/concurrency-go/chapter5/workerpool/wpTwo"
+	"github.com/victoryRo/concurrency-go/chapter1"
 )
 
 func main() {
+	chapter1.Execute()
 	// chapter1.ExecuteOne()
 	// fmt.Println("-----------------------")
 	// chapter1.ExecuteTwo()
@@ -20,5 +21,5 @@ func main() {
 	// chanel.LocalMain() //no results
 	// simple.Localmain()
 	// wpone.LocalMain()
-	wptwo.LocalMain()
+	// wptwo.LocalMain()
 }
